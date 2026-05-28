cbb4bfaa7f2ccf89b3d89ad9755253e90479fbac
Sahil Parmar <sahilparmar8850@gmail.com>
2026-05-28 22:37:01 +0530

    Update db.py

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,43 +1,11 @@
+import os
 import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Ok1ok2ok3#",
-        database="crm_db"
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT", 3306))
     )
-
-def init_db():
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS tickets (
-        id VARCHAR(50) PRIMARY KEY,
-        customer_name VARCHAR(255),
-        customer_email VARCHAR(255),
-        issue_title VARCHAR(255),
-        issue_description TEXT,
-        status VARCHAR(50),
-        priority VARCHAR(50),
-        created_at DATETIME,
-        updated_at DATETIME,
-        notes TEXT
-    )
-    """)
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS activity_logs (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        ticket_id VARCHAR(50),
-        action VARCHAR(100),
-        detail TEXT,
-        timestamp DATETIME
-    )
-    """)
-
-    conn.commit()
-    cursor.close()
-    conn.close()
-    print("Database initialized")
